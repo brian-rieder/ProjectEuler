32dd3ad556356542364bc331e9ea216599aa2232
Brian Rieder <brieder@purdue.edu>
2014-12-08 15:46:24 -0500

    Added problem statement

--- PE08_LargestProductInSeries.py
+++ PE08_LargestProductInSeries.py
@@ -1,5 +1,7 @@
 __author__ = 'Brian Rieder'
 
+# Problem 8: Largest Product in Series
+
 # The four adjacent digits in the 1000-digit number that have the greatest product are 9 × 9 × 8 × 9 = 5832.
 # Find the thirteen adjacent digits in the 1000-digit number that have the greatest product. What is the value
 # of this product?

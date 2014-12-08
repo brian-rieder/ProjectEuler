a4bcd6a813c7227dcfe396c867e5210d5e4289dd
Brian Rieder <brieder@purdue.edu>
2014-12-08 15:48:18 -0500

    Fixed problem statement

--- PE11_LargestProductInGrid.py
+++ PE11_LargestProductInGrid.py
@@ -23,4 +23,5 @@ __author__ = 'Brian Rieder'
 # 20 73 35 29 78 31 90 01 74 31 49 71 48 86 81 16 23 57 05 54
 # 01 70 54 71 83 51 54 69 16 92 33 48 61 43 52 01 89 19 67 48
 #
-# What is the greatest product of four adjacent numbers in the same direction (up, down, left, right, or diagonally) in the 20×20 grid?
+# What is the greatest product of four adjacent numbers in the same direction (up, down, left, right, or diagonally)
+# in the 20×20 grid?

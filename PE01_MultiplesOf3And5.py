10b03fa2c37ad9ad62ba01b420c8875576400bc6
Brian Rieder <brieder@purdue.edu>
2014-12-09 01:17:04 -0500

    Full solution

--- PE01_MultiplesOf3And5.py
+++ PE01_MultiplesOf3And5.py
@@ -7,3 +7,27 @@ __author__ = 'Brian Rieder'
 # Find the sum of all the multiples of 3 or 5 below 1000.
 
 # Answer: 233168
+
+
+def gen_sum_multiples5(num):
+    sum_val = 0
+    mult_val = 0
+    while mult_val < num:
+        if mult_val % 3 != 0:
+            sum_val += mult_val
+        mult_val += 5
+    return sum_val
+
+
+def gen_sum_multiples3(num):
+    sum_val = 0
+    mult_val = 0
+    while mult_val < num:
+        sum_val += mult_val
+        mult_val += 3
+    return sum_val
+
+
+testval = 1000
+print("Sum of all tested values for " + str(testval) + " are: " + str(gen_sum_multiples3(testval) +
+                                                                      gen_sum_multiples5(testval)))

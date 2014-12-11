14fa7ef0969b7f9b2b7c4608aeeea1c63a9ce108
Brian Rieder <brieder@purdue.edu>
2014-12-11 12:29:36 -0500

    Initial solution -- doesn't work

--- PE05_SmallestMultiple.py
+++ PE05_SmallestMultiple.py
@@ -8,3 +8,18 @@ __author__ = 'Brian Rieder'
 # What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 
 # Answer: 232792560
+
+num = 20
+while True:
+    if num % 2 == 0:
+        if num % 3 == 0:
+            if num % 5 == 0:
+                if num % 7 == 0:
+                    print num
+                    if num % 11 == 0:
+                        if num % 13 == 0:
+                            if num % 17 == 0:
+                                if num % 19 == 0:
+                                    print num
+                                    break
+    num += 20

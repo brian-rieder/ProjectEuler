febb74f8e15ab1e5e08c0a2f458da204af401d45
Brian Rieder <brieder@purdue.edu>
2014-12-11 10:53:47 -0500

    Full solution

--- PE02_EvenFibonacci.py
+++ PE02_EvenFibonacci.py
@@ -11,3 +11,24 @@ __author__ = 'Brian Rieder'
 # even-valued terms.
 
 # Answer: 4613732
+
+
+def fibonacci(val):
+    if val == 0:
+        return 1
+    elif val == 1:
+        return 1
+    return fibonacci(val-1) + fibonacci(val-2)
+
+
+fib_val = 0
+itr = 0
+sum_val = 0
+while fib_val < 4000000:
+    fib_val = fibonacci(itr)
+    if fib_val % 2 == 0:
+        sum_val += fib_val
+    itr += 1
+    
+
+print sum_val

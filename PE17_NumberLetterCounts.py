0cc27316dafcd7644bcca237c34e85b5017b4248
Brian Rieder <brieder@purdue.edu>
2014-12-15 15:08:36 -0500

    Incorrect...

--- PE17_NumberLetterCounts.py
+++ PE17_NumberLetterCounts.py
@@ -8,3 +8,27 @@ __author__ = 'Brian Rieder'
 # If all the numbers from 1 to 1000 (one thousand) inclusive were written out in words, how many letters would be used?
 
 # Answer: 21124
+
+single = [0, 3, 3, 5, 4, 4, 3, 5, 5, 4, 3, 6, 6, 8, 8, 7, 7, 9, 8, 8]
+decimal = [0, 3, 6, 6, 5, 5, 5, 7, 6, 6]
+hundred = 7
+thousand = 8
+
+total = 0
+max_val = 5
+
+# Cover all of the 1 - 19s
+for i in range(0, 20):
+    total += single[i]
+
+total *= 9
+
+# Cover all decimal values
+for i in range(0, 10):
+    total += decimal[i] * 100
+
+total += hundred * 900
+
+total += thousand
+
+print(total)

1b5fd5c6f90319ef4b0a3b1cbdedbe525a993c5d
Brian Rieder <brieder@purdue.edu>
2014-12-15 18:06:05 -0500

    Full solution

--- PE21_AmicableNumbers.py
+++ PE21_AmicableNumbers.py
@@ -3,7 +3,7 @@ __author__ = 'Brian Rieder'
 # Problem 21: Amicable Numbers
 
 # Let d(n) be defined as the sum of proper divisors of n (numbers less than n which divide evenly into n).
-# If d(a) = b and d(b) = a, where a ≠ b, then a and b are an amicable pair and each of a and b are called
+# If d(a) = b and d(b) = a, where a != b, then a and b are an amicable pair and each of a and b are called
 # amicable numbers.
 #
 # For example, the proper divisors of 220 are 1, 2, 4, 5, 10, 11, 20, 22, 44, 55 and 110; therefore d(220) = 284.
@@ -11,26 +11,27 @@ __author__ = 'Brian Rieder'
 #
 # Evaluate the sum of all the amicable numbers under 10000.
 
+from math import floor
+
 
 def sum_proper_divisors(num):
     test_val = 1
-    sum = 0
-    while test_val <= (num/2):
+    sum_val = 0
+    while test_val <= floor(num/2):
         if num % test_val == 0:
-            sum += test_val
+            sum_val += test_val
         test_val += 1
-    return sum
+    return sum_val
 
 
-# This is wrong.... we want all amicable under 10000
-def gen_10000_proper():
+def gen_amicable_under_10000():
     a1 = []
-    for i in range(0, 999):
-        a1.append(sum_proper_divisors(i))
+    for i in range(0, 10000):
+        check = sum_proper_divisors(i)
+        if check != i:
+            if i == sum_proper_divisors(check):
+                a1.append(sum_proper_divisors(i))
     return a1
 
 
-# This isn't done.
-def check_amicable(array):
-    if 1:
-        return array
+print(sum(gen_amicable_under_10000()))

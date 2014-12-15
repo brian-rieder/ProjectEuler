9c3fe5ceedcd1abf6088ad78be5168fbedab51c5
Brian Rieder <brieder@purdue.edu>
2014-12-15 04:24:34 -0500

    Recursive and mathematical solution - Credit to Jason Hill for mathematical proof

--- PE15_LatticePaths.py
+++ PE15_LatticePaths.py
@@ -7,3 +7,31 @@ __author__ = 'Brian Rieder'
 #
 # How many such routes are there through a 20×20 grid?
 
+grid_dime = [20, 20]
+
+
+def recursive_approach(grid_dimension):
+    if grid_dimension == [0, 0]:
+        return 1
+    path_val = 0
+    if grid_dimension[0] > 0:
+        path_val += recursive_approach([grid_dimension[0]-1, grid_dimension[1]])
+    if grid_dimension[1] > 0:
+        path_val += recursive_approach([grid_dimension[0], grid_dimension[1]-1])
+    return path_val
+
+
+def mathematical_approach(grid_dimension):
+    grid_val = [[i+1 for i in range(1, grid_dimension + 1)] for j in range(1, grid_dimension + 1)]
+    for i in range(0, grid_dimension):
+        grid_val[i][0] = i + 2
+    for row in range(1, grid_dimension):
+        for column in range(1, grid_dimension):
+            grid_val[row][column] = grid_val[row - 1][column] + grid_val[row][column - 1]
+    return grid_val[grid_dimension - 1][grid_dimension - 1]
+
+
+
+result = mathematical_approach(20)
+
+print(result)

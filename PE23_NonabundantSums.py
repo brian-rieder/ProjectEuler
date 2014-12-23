6106c3603648cf6f5a20a8bf1fecdca8c8a1b8b1
Brian Rieder <brieder@purdue.edu>
2014-12-23 01:16:34 -0500

    Two inefficient, but potentially correct, solutions.

--- PE23_NonabundantSums.py
+++ PE23_NonabundantSums.py
@@ -19,9 +19,50 @@ __author__ = 'Brian Rieder'
 
 def sum_proper_divisors(num):
     test_val = 1
-    sum = 0
+    sum_val = 0
     while test_val <= (num/2):
         if num % test_val == 0:
-            sum += test_val
+            sum_val += test_val
         test_val += 1
-    return sum
+    return sum_val
+
+
+def is_abundant(num):
+    check = sum_proper_divisors(num)
+    if check > num:
+        return True
+    else:
+        return False
+
+
+def is_summable_by_abundant(num):
+    for j in range(12, int(num/2) + 1):
+        if is_abundant(j):
+            if is_abundant(num - j):
+                return True
+    return False
+
+
+# final_sum = 0
+# for i in range(24, 28124):
+#     if is_summable_by_abundant(i):
+#         final_sum += i
+#
+# print("Final sum:" + final_sum)
+
+
+abundant = []
+for i in range(12, 28125):
+    if is_abundant(i):
+        abundant.append(i)
+
+hash_list = [False] * 28125
+for i in hash_list:
+    for k in hash_list:
+        if abundant[i] + abundant[k] <= 28124:
+            hash_list[abundant[i] + abundant[k]] = True
+
+final_sum = 0
+for i in hash_list:
+    if not hash_list:
+        final_sum += i

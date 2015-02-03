9c8ee7bb576117764964f67cb87f364a8ff9d931
Brian Rieder <brian-rieder@users.noreply.github.com>
2015-02-03 15:52:29 -0500

    Full solution -- allows input of amount to break down

--- PE31_CoinSums.py
+++ PE31_CoinSums.py
@@ -10,4 +10,36 @@ __author__ = 'Brian Rieder'
 # 1×£1 + 1×50p + 2×20p + 1×5p + 1×2p + 3×1p
 # How many different ways can £2 be made using any number of coins?
 
-# Answer:
+# Answer: 73682
+
+# This will cause immense recursion depth or stack overflow, so should not be used
+def recursively_calculate_num_combinations(remaining):
+    if remaining < 0:
+        return
+    if remaining == 0:
+        global combinations
+        combinations += 1
+        return
+    recursively_calculate_num_combinations(remaining - 1)
+    recursively_calculate_num_combinations(remaining - 2)
+    recursively_calculate_num_combinations(remaining - 5)
+    recursively_calculate_num_combinations(remaining - 10)
+    recursively_calculate_num_combinations(remaining - 20)
+    recursively_calculate_num_combinations(remaining - 50)
+    recursively_calculate_num_combinations(remaining - 100)
+    recursively_calculate_num_combinations(remaining - 200)
+
+
+def iteratively_calculate_num_combinations(amount):
+    number_of_ways = [0] * (amount + 1)
+    number_of_ways[0] = 1
+    for coin_value in [1, 2, 5, 10, 20, 50, 100, 200]:
+        for index in range(coin_value, (amount + 1)):
+            number_of_ways[index] += number_of_ways[index - coin_value]
+    print(number_of_ways[amount])
+
+
+if __name__ == "__main__":
+    breakdown_amount = int(float(input("Break down what amount of currency (£): ")) * 100)
+    combinations = 0
+    iteratively_calculate_num_combinations(breakdown_amount)
